7cfb8d335f6d30b554bbc63f4c1360c4925580cc
galimsarov <galimsarov@pss.ru>
2024-01-09 09:43:00 +0500

    Initial commit

---
+++ src/main/java/pss/mira/orp/JavaOCAOCPP/JavaOcaOcppApplication.java
@@ -0,0 +1,13 @@
+package pss.mira.orp.JavaOCAOCPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOcaOcppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOcaOcppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pss/mira/orp/JavaOCAOCPP/JavaOcaOcppApplicationTests.java
@@ -0,0 +1,13 @@
+package pss.mira.orp.JavaOCAOCPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOcaOcppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
